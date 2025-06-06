89530c3105f091d69e6dc4c7e6c0469d41f5db1c
mitchdmarino <mitchdmarino@gmail.com>
2025-06-05 21:34:33 -0700

    Date filters to transaction data

--- Budget App/client/src/components/transactions/TransactionList.jsx
+++ Budget App/client/src/components/transactions/TransactionList.jsx
@@ -1,25 +1,44 @@
 import TransactionListComponent from "./TransactionListComponent";
 import { useEffect, useState } from "react";
 
+// set the initial month and year filter
+const today = new Date();
+let month = today.getMonth();
+let year = today.getFullYear();
+let dateRef = new Date(year, month, 1); // initial date filter
+
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
 export default function TransactionList({transactions}) {
     const [page, setPage] = useState(1);
     const [currentTransactions, setCurrentTransactions] = useState([]);
-    const [monthFilter, setMonthFilter] = useState(null);
-    const [yearFilter, setYearFilter] = useState(null);
+    const [dateFilter, setDateFilter] = useState(dateRef);
 
-    
     useEffect(() => {
+        // filter transactions for the selected date (using the month and year)
+        let dateFilterStart = dateFilter
+        let dateFilterEnd = new Date(dateFilter.getFullYear(), dateFilter.getMonth()+1, 1);
+        transactions = transactions.filter(transaction => {
+            let thisDate = transaction.txnDate;
+            return new Date(thisDate).getTime() >=new Date(dateFilterStart).getTime() && new Date(thisDate).getTime()< new Date(dateFilterEnd).getTime(); // don't include end date 
+        })
         const itemsPerPage = 25;
         let end = itemsPerPage * page;
         let start = end - itemsPerPage;
         setCurrentTransactions(transactions.slice(start, end));
-    }, [page])
+    }, [page, dateFilter])
     let transactionMap = currentTransactions.map(transaction => {
         return (
             <TransactionListComponent key={transaction._id} transaction={transaction} />
         )
     })
     
+    const handleNextDate = () => {
+        setDateFilter(new Date(dateFilter.getFullYear(), dateFilter.getMonth() + 1, 1));
+    }
+    const handlePrevDate = () => {
+        setDateFilter(new Date(dateFilter.getFullYear(), dateFilter.getMonth() - 1, 1));
+    }
     const handleNextPage = () => {
         if (transactions.length / page >=1) setPage(page + 1);
     } 
@@ -29,10 +48,11 @@ export default function TransactionList({transactions}) {
 
     if (!transactions || transactions.length === 0) return null;
     return (
-        <>
-            <div className="page-selection">
-                <button onClick={handlePrevPage}>{'<'}</button>
-                <button onClick={handleNextPage}>{'>'}</button>
+        <div className="transaction-list-container">
+            <div className="transaction-date-selection">
+                <button onClick={handlePrevDate}>{'<'}</button>
+                <h1>{months[dateFilter.getMonth()]} {dateFilter.getFullYear()}</h1>
+                <button onClick={handleNextDate}>{'>'}</button>
             </div>
             <div className="transaction-list">
                 <div className="transaction-list-header">
@@ -43,6 +63,10 @@ export default function TransactionList({transactions}) {
                 </div>
                 {transactionMap}
             </div>
-        </>
+            <div className="page-selection">
+                <button onClick={handlePrevPage}>{'<'}</button>
+                <button onClick={handleNextPage}>{'>'}</button>
+            </div>
+        </div>
     )
 }
